7f55b3f13eccf6cae117978fee6ebb2ba73cf44f
mm-dantas <matheusgithub@outlook.com>
2024-09-30 17:41:16 -0300

    Atualização do projeto com HashRouter e ajustes para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./Sprint3-Web-Dev/",
+  base: "/Sprint3-Web-Dev/",
 })
